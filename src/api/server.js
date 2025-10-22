313bcbb82f4c880381bd35ed3f61a4502b3d03a3
Mirza Raheel <raheel1195@gmail.com>
2025-10-22 16:18:59 +0500

    changed server.js again with express

--- src/api/server.js
+++ src/api/server.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  return res.status(200).json({ message: "✅ Pure function working" });
-}
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({ message: "✅ Express minimal working" });
+});
+
+export const handler = serverless(app);
